ffa6bedc93e587f996f7866b0abbc2f9cc284133
csae8092 <peter.andorfer@oeaw.ac.at>
2020-01-30 11:52:44 +0100

    accordion form

--- myprodigy/forms.py
+++ myprodigy/forms.py
@@ -72,13 +72,13 @@ class NerSampleFilterFormHelper(FormHelper):
         self.helper.form_tag = False
         self.add_input(Submit('Filter', 'Search'))
         self.layout = Layout(
-            Fieldset(
-                'Basic search options',
-                'entities',
-                'dataset',
-                css_id="basic_search_fields"
-                ),
             Accordion(
+                AccordionGroup(
+                    'Basic search options',
+                    'entities',
+                    'dataset',
+                    css_id="basic_search_fields"
+                ),
                 AccordionGroup(
                     'Advanced search',
                     'id',
