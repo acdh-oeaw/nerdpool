bf6e4af0494f36bc3e0a9103a4b1b9f4ed3b8da0
csae8092 <p.andorfer@gmail.com>
2020-12-07 08:54:49 +0100

    added span to NerSample Serialisation

--- myprodigy/serializers.py
+++ myprodigy/serializers.py
@@ -4,9 +4,13 @@ from . models import NerSample, NerDataSet
 
 
 class NerSampleSerializer(serializers.ModelSerializer):
+
+    spans = serializers.ListField(source="get_spans")
+
     class Meta:
         model = NerSample
         fields = [
             'id',
             'text',
+            'spans',
         ]
